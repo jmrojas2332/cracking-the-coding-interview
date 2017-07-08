5760ab106017c2ca095cd68f5ca448559654ec86
Jose Rojas <jmrojas2332@gmail.com>
2017-07-07 23:05:27 -0500

    Made solution more efficient by tracking odd_count while traversing string

--- python/1-4.py
+++ python/1-4.py
@@ -14,55 +14,58 @@ import unittest
 ALPHABET_SIZE = 256
 
 def palindrome_permutation(string):
-	"""
-	Function to check if a string is a palindrome permution
-	Firse check how many times every character in our alphabet appears in the
-	string. Then return false if more than only 1 character has an odd count.
-	Otherwise, we return false.
+    """
+    Function to check if a string is a palindrome permution
 
-	Note: We are using extended ascii as our alphabet (see ALPHABET_SIZE) and
-		  our solution handles lowercase and uppercase as different characters
-	"""
+    Check how many times each unique character appears in the string. Return
+    False if more than 1 character has an odd count, otherwise, return True. 
 
-	
+    An odd_count is used while traversing the string to track the amount of
+    characters that have an odd count. This prevents having to iterate through
+    char_count because we already know how many characters have an odd count.
 
-	char_count = [0] * ALPHABET_SIZE
+    Note: We are using extended ascii as our alphabet (see ALPHABET_SIZE) and
+    our solution handles lowercase and uppercase as different characters.
 
-	for c in string:
-		val = ord(c)
-		if val >= ALPHABET_SIZE:
-			raise IndexError('Character {0} with value {1} not in our alphabet of size {2}'.format(
-				c,
-				val, 
-				ALPHABET_SIZE))
-		else:
-			char_count[val] += 1
+    Time Complexity: O(n) where n is the length of the string
+    Space Complexity: O(k) where k is the size of the alphabet
+    """	
+    
+    char_count = [0 for _ in range(0, ALPHABET_SIZE)]
+    odd_count = False
 
-	odd_found = False
-	for count in char_count:
-		if count % 2 == 1:
-			if odd_found:
-				return False
-			else:
-				odd_found = True
+    for c in string:
+        val = ord(c)
+        if val >= ALPHABET_SIZE:
+            raise IndexError('Character {0} with value {1} not in our alphabet of size {2}'.format(
+                c,
+                val, 
+                ALPHABET_SIZE))
+        else:
+            char_count[val] += 1
 
-	return True
+            if char_count[val] % 2 == 1:
+                odd_count += 1
+            else:
+                odd_count -= 1
+
+    return odd_count <= 1 
 
 
 
 class Test(unittest.TestCase):
-	data_expected_true = ['aabb', 'aabbc', '01010101', ' ', 'a', 'hello hello']
-	data_expected_false = ['aaab', 'aabbcd', '010101', 'Hello hello']
+    data_expected_true = ['aabb', 'aabbc', '01010101', ' ', 'a', 'hello hello']
+    data_expected_false = ['aaab', 'aabbcd', '010101', 'Hello hello']
 
-	def test_palindrome_permutation(self):
-		for test_string in self.data_expected_true:
-			actual = palindrome_permutation(test_string)
-			self.assertTrue(actual)
+    def test_palindrome_permutation(self):
+    	for test_string in self.data_expected_true:
+    		actual = palindrome_permutation(test_string)
+    		self.assertTrue(actual)
 
-		for test_string in self.data_expected_false:
-			actual = palindrome_permutation(test_string)
-			self.assertFalse(actual)
+    	for test_string in self.data_expected_false:
+    		actual = palindrome_permutation(test_string)
+    	self.assertFalse(actual)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
