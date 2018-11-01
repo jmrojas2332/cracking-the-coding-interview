3c7d204e4422d5eb586664e4ff32f52f533511dd
jmrojas2332 <jmrojas2332@gmail.com>
2018-10-31 23:05:35 -0500

    1-1 solution + eslint and autocomplete setup

---
+++ javascript/ch1_arrays_and_strings/1-1.js
@@ -0,0 +1,41 @@
+/*
+Cracking the Coding Interview Question 1.1 Is Unique
+
+Implement an algorithm to determine if a string has all unique characters. What
+if you cannot use additional data structures?
+*/
+
+var assert = require('chai').assert;
+
+function isUnique(str) {
+    return new Set(str).size == str.length;  // set does not store duplicates
+}
+
+function isUniqueNoDataStructues(str) {  // trades speed for space
+    for (let i = 0; i < str.length - 1; ++i) {
+        for (let j = i + 1; j < str.length; ++j) {
+            if (str.charAt(i) == str.charAt(j)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+describe('isUnique', function() {
+    let dataExpectedTrue = ['abcd', 'jose', 'rojas', ' '];
+    let dataExpectedFalse = ['hello', 'good', 'buddy', '  '];
+
+    for (let str of dataExpectedTrue) {
+        it(`checks if ${str} is unique`, function() {
+            assert.isTrue(isUniqueNoDataStructues(str));
+        });
+    }
+
+    for (let str of dataExpectedFalse) {
+        it(`checks if ${str} is not unique`, function() {
+            assert.isFalse(isUniqueNoDataStructues(str));
+        });
+    }
+});
